086e555022a3b18f317f9487bf6d2e88d28e964d
Rafilde <rafasilva182003@gmail.com>
2025-04-22 15:48:53 -0300

    feat: updated the animation

--- jogo_conscientizacao_posparto/Assets/Scripts/PlayerController.cs
+++ jogo_conscientizacao_posparto/Assets/Scripts/PlayerController.cs
@@ -6,35 +6,69 @@ public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 3;
     Animator animator;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector3 movement;
+    private CharacterController playerController;
+
     void Start()
     {
         animator = GetComponent<Animator>();
+        //playerController = GetComponent<CharacterController>();
+        //animator.applyRootMotion = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // movement.Set(-Input.GetAxisRaw("Horizontal"), 0, -Input.GetAxisRaw("Vertical"));
+        // playerController.Move(movementSpeed * Time.deltaTime * movement); isso vai fazer ele andar papai
+        // if (movement != Vector3.zero) {
+        //     transform.forward = movement;
+        // }
+        
+        movement.Set(-Input.GetAxisRaw("Horizontal"), 0, -Input.GetAxisRaw("Vertical"));
+        if (movement != Vector3.zero) {
+            transform.forward = movement;
+        }
         ControllPlayer();
     }
 
-    void ControllPlayer() 
+    void ControllPlayer()
     {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        float moveVertical = Input.GetAxisRaw("Vertical");
+        bool isMoving = movement != Vector3.zero;
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        if (movement != Vector3.zero)
+        if (isMoving && stateInfo.IsName("MainCharacterIdle"))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
-            animator.SetInteger("Walk", 1);
+            animator.SetInteger("PreJump", 1);
         }
-        else 
+        else if (!isMoving && stateInfo.IsName("MainCharacterPreJump"))
         {
-            animator.SetInteger("Walk", 0);
+            animator.SetInteger("PreJump", 0);
         }
 
-        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+        if (stateInfo.IsName("MainCharacterPreJump") && isMoving)
+        {
+            animator.SetInteger("Jump", 1);
+        }
+
+        if (stateInfo.IsName("MainCharacterJump"))
+        {
+            //animator.SetInteger("Jump", 0);
+            animator.SetInteger("Landing", 1);
+        }
+
+
+        if (stateInfo.IsName("MainCharacterLanding"))
+        {
+            if (!isMoving)
+            {
+                animator.SetInteger("PreJump", 0);
+                animator.SetInteger("Jump", 0);
+                animator.SetInteger("Landing", 0);
+            }
+            else
+            {
+                animator.SetInteger("PreJump", 2);
+            }
+        }
     }
 }
